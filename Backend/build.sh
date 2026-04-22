07467861628c4353ba47af265844efeab482206b
Vaish-251105 <vaishnavisingh251105@gmail.com>
2026-04-22 12:03:11 +0530

    Upgrade pip and install build dependenciesMake build.sh more robust and upgrade pip/setuptools

--- Backend/build.sh
+++ Backend/build.sh
@@ -2,10 +2,14 @@
 # exit on error
 set -o errexit
 
-# Install setuptools first to handle pkg_resources dependencies (like razorpay)
-pip install setuptools
+echo "Upgrading pip and installing core build dependencies..."
+pip install --upgrade pip setuptools wheel
 
+echo "Installing requirements..."
 pip install -r requirements.txt
 
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Running migrations..."
 python manage.py migrate
